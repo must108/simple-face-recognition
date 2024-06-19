2fce372fab617bb0fcfd65a1101ccad453836dd1
mustaeen <136145181+must108@users.noreply.github.com>
2024-06-19 17:57:58 -0400

    favicon fix?

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import type { Viewport } from 'next';
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -7,7 +8,17 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Face Detection Tool :D",
   description: "Detect faces in pictures!",
+  icons: {
+    icon: 'favicon.ico'
+  },
 };
+
+export const viewport: Viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
+  userScalable: false,
+}
 
 export default function RootLayout({
   children,
@@ -16,7 +27,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={`{inter.className} bg-slate-900`}>
         {children}
         </body>
     </html>
